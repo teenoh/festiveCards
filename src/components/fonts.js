04643bbf6ab10a72d4830696313361daca8461b8
teenoh <valentino@grit.systems>
2017-12-26 08:00:52 +0100

    option to change text color and font

---
+++ src/components/fonts.js
@@ -0,0 +1,21 @@
+
+const font = [
+    {
+        name: "Trajan Bold",
+        value: "TrajanBold"
+    },
+    {
+        name: "Trajan Regular",
+        value: "TrajanRegular"
+    },
+    {
+        name: "Bebas Regular",
+        value: "bebasregular"
+    },
+    {
+        name: "Bebas Neue",
+        value: "bebas_neue_regular"
+    }
+] 
+
+export default font
